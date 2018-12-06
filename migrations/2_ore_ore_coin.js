464da065e7ad9cd6df4168c814e493d24ed67b2c
k33asby <k33asby@gmail.com>
2018-12-06 16:13:35 +0900

    エスクローを追加

--- migrations/2_ore_ore_coin.js
+++ migrations/2_ore_ore_coin.js
@@ -2,11 +2,13 @@ var OreOreCoin = artifacts.require("./OreOreCoin.sol");
 var Owned = artifacts.require("./Owned.sol");
 var Members = artifacts.require("./Members.sol");
 var Crowdsale = artifacts.require("./Crowdsale.sol");
+var Escrow = artifacts.require("./Escrow.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Owned);
   deployer.deploy(Members);
   deployer.deploy(OreOreCoin, 10000, "OreOreCoin", "oc", 0).then(function() {
-    return deployer.deploy(Crowdsale, 10, 5000, 100, OreOreCoin.address);
+    deployer.deploy(Crowdsale, 10, 5000, 100, OreOreCoin.address);
+    deployer.deploy(Escrow, OreOreCoin.address, 10, 2000);
   });
 };
